432974490fa0281395c0fccc4b7c8ea6a14cdb9b
snag <somjit.nag@in.imshealth.com>
2020-02-20 14:48:35 +0530

    UseEffect: run conditionally

--- src/components/useEffect/HookOne.tsx
+++ src/components/useEffect/HookOne.tsx
@@ -3,16 +3,18 @@ import React, { useState, useEffect } from "react";
 const HookOne = () => {
   const initialCount = 0;
   const [count, setCount] = useState(initialCount);
+  const [name, setName] = useState("");
 
   useEffect(() => {
-    // run this after every render
-    document.title = `Clicked ${count} times`
-  })
+    console.log('UseEffect: Updating document Title');    
+    document.title = `Clicked ${count} times`;
+  }, [count]); // only render when 'count' changes
 
   return (
     <div>
       Count: {count}
       <br />
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
       <button onClick={() => setCount(initialCount)}>Reset</button>
       <button onClick={() => setCount(count + 1)}>increment Count</button>
     </div>
